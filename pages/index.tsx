1353a5ffb1562c23a3ff02d792e6a37ae563976c
Fusioz <trongtin.0301@gmail.com>
2022-12-08 21:09:54 +0700

    Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,39 @@ import { useState } from 'react'
 
 const API_KEY = "d43e193aab8791bd97f1cac218090e18"
 
-const Home: NextPage = () =>
-{  
+const Home: NextPage = () => {  
   const [cityInput, setCityInput] = useState("");
 
-  async function getWeathterData() 
-  {
+  const [weatherData, setWeatherData] = useState<any>({}); {city: "Seattle"}
+
+  async function getWeathterData() {
     console.log("Button pressed");
+    // query data if there is an error, throw
+    // If not, save data
+    try {
+      const serverResponse = await fetch (
+        "https://api.openweathermap.org/data/2.5/weather?" +
+        "q=" +
+        cityInput +
+        "&appid=" +
+        API_KEY +
+        "&units=imperial"
+      )
+      const data = await serverResponse.json();
+      console.log(data);
+      if (data?.cod === "400") throw data;
+      setWeatherData(data);
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
+
+  console.log(cityInput);
 
   return (
     <div
-      style =
-      {{
+      style = {{
         position: 'static',
         height: '100vh',
         backgroundImage: "url('https://littlevisuals.co/images/canal.jpg')",
@@ -32,8 +52,7 @@ const Home: NextPage = () =>
       }}
       >
         <div
-          style = 
-          {{
+          style = {{
             position: 'absolute',
             left: '50%',
             top: '50%',
@@ -54,19 +73,40 @@ const Home: NextPage = () =>
           <Group position= 'apart'>
             <TextInput
             label = "City Name"
-            placeholder="ex: San Dieago"
+            placeholder="ex: Ho Chi Minh"
             onChange={(e) => setCityInput(e.target.value)} // C -> setCityInput("C")  
             />
           </Group>
           <Group position= 'apart'>
-            <Button variant = "gradient" size = "md" onClick={() => getWeathterData}>
+            <Button variant = "gradient" size = "md" onClick={() => getWeathterData()}>
               Get Weather
             </Button>
           </Group>
+          {Object.keys(weatherData).length !== 0 ?
+            <>
+              <Group position= 'left'>
+                <Text>
+                  {weatherData.name} Weather                 
+                </Text>
+              </Group>
+              <Group position= 'left'>
+                
+                <img
+                  src = {"http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@4x.png"}
+                  width = "100px"
+                  height= "100px"
+                />
+
+                <Text size = "lg" weight ={500}>
+                  Currently {weatherData.main.temp} &deg;F                 
+                </Text>
+              </Group>
+            </>
+            : null
+          }
         </Paper>
         </div>
     </div>
-  
   )
 }
 
